da13ac310442cc5c1994ad827c93b4c2a7816812
mbarto202 <michael.d.barto@gmail.com>
2024-08-28 21:39:36 -0400

    access modifiers

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // classes
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // client: string;
+    // details: string;
+    // amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes ${this.amount} for ${this.details}`;
@@ -15,7 +18,10 @@ const invTwo = new Invoice('Rachel', 'gym', 20);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
+//--------------------
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
