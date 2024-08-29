b5b128244cbee7c1381a544b2ca49a2c140a011b
mbarto202 <michael.d.barto@gmail.com>
2024-08-29 08:11:29 -0400

    interfaces

--- public/app.js
+++ public/app.js
@@ -1,3 +1,18 @@
+const me = {
+    name: 'Shaun',
+    age: 30,
+    speak: (text) => {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
 import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('Michael', 'gym', 25);
 const invTwo = new Invoice('Rachel', 'gym', 20);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'Shaun',
+    age: 30,
+    speak:(text: string): void {
+        console.log(text);
+        
+    },
+    spend(amount: number) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello', person.name);
+}
+
+greetPerson(me)
+
+
 import {Invoice} from './classes/Invoice.js'
 
 const invOne = new Invoice('Michael', 'gym', 25)
